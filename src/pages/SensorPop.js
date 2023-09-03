5541c06c875449d8b1d1b0dc19db44ad19dffd30
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-09-03 12:58:01 +0530

    add request handling to api

--- src/pages/SensorPop.js
+++ src/pages/SensorPop.js
@@ -4,12 +4,44 @@ import React, { useEffect, useState } from 'react';
 
 const SensorPopup = ({ deviceId, onClose }) => {
     const [loading, setLoading] = useState(true)
-    const [sensorValue, setSensorValue] = useState()
+    const [sensorParams, setSensorParams] = useState([])
+
+    const fetchSensorValues = async () => {
+        try {
+          const response = await fetch("http://localhost:4001/api/get-sensor-params", {
+            method: "GET",
+            headers: { device_id:   deviceId }
+          });
+          const jsonData = await response.json();
+          setSensorParams(jsonData);
+          console.log(jsonData);
+        } catch (err) {
+          console.error(err.message);
+        } finally {
+          setLoading(false);
+        }
+      };
     
+      useEffect(() => {
+        fetchSensorValues();
+      }, []);
   return (
     <div className="popup">
       <div className="popup-content">
         <h2>Device ID: {deviceId}</h2>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : sensorParams.length > 0 ? (
+          <ul className="">
+            {sensorParams.map((sv) => (
+              <li
+                key={sv.sensor_id}
+              >{sv.key}</li>
+            ))}
+          </ul>
+        ) : (
+          <h1>No Sensors</h1>
+        )}
         <button onClick={onClose}>Close</button>
       </div>
     </div>
