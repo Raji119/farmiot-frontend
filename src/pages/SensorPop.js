1478a642d59f30c4b4d998a69b3259514647f512
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-09-03 20:10:14 +0530

    detached sensorvalue component

--- src/pages/SensorPop.js
+++ src/pages/SensorPop.js
@@ -1,55 +1,34 @@
 import React, { useEffect, useState } from 'react';
-
+import SensorValuePopup from './SensorValuePopup'; // Import the new component
 
 const SensorPopup = ({ deviceId, onClose }) => {
-    const [loading, setLoading] = useState(true)
-    const [sensorParams, setSensorParams] = useState([])
-    const [sensorValues, setSensorValues] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [sensorParams, setSensorParams] = useState([]);
+  const [selectedSensorId, setSelectedSensorId] = useState(null);
 
-    const fetchSensorParams = async () => {
-        try {
-          const response = await fetch("http://localhost:4001/api/get-sensor-params", {
-            method: "GET",
-            headers: { device_id: deviceId }
-          });
-          const jsonData = await response.json();
-          setSensorParams(jsonData);
-          // console.log(jsonData);
-        } catch (err) {
-          console.error(err.message);
-        } finally {
-          setLoading(false);
-        }
-      };
+  const fetchSensorParams = async () => {
+    try {
+      const response = await fetch("http://localhost:4001/api/get-sensor-params", {
+        method: "GET",
+        headers: { device_id: deviceId }
+      });
+      const jsonData = await response.json();
+      setSensorParams(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-      const fetchSensorValue = async(sensorId) => {
-        try {
-          const response = await fetch("http://localhost:4001/api/get-sensor-value", {
-            method: "GET",
-            headers: { sensor_id: sensorId }
-          });
-          const jsonData = await response.json();
-          setSensorValues(jsonData);
-          // console.log(jsonData[0].value);
-          console.log(sensorValues[0].value);
-          alert(sensorValues[0].value)
-        } catch (err) {
-          console.error(err.message);
-        } 
-      }
-    
-      useEffect(() => {
-        fetchSensorParams();
-      }, []);
+  useEffect(() => {
+    fetchSensorParams();
+  }, []);
 
-      useEffect(() => {
-        // This effect will be triggered whenever sensorValues changes
-        if (sensorValues.length > 0) {
-          console.log(sensorValues[0].value);
-          alert(sensorValues[0].value);
-        }
-      }, [sensorValues]);
-    
+  const handleSensorClick = (sensorId) => {
+    setSelectedSensorId(sensorId);
+  };
+
   return (
     <div className="popup">
       <div className="popup-content">
@@ -61,7 +40,7 @@ const SensorPopup = ({ deviceId, onClose }) => {
             {sensorParams.map((sv) => (
               <button
                 key={sv.sensor_id}
-                onClick={() => fetchSensorValue(sv.sensor_id)}
+                onClick={() => handleSensorClick(sv.sensor_id)}
               >{sv.key}</button>
             ))}
           </ul>
@@ -69,6 +48,12 @@ const SensorPopup = ({ deviceId, onClose }) => {
           <h1>No Sensors</h1>
         )}
         <button onClick={onClose}>Close</button>
+        {selectedSensorId !== null && (
+          <SensorValuePopup
+            sensorId={selectedSensorId}
+            onClose={() => setSelectedSensorId(null)}
+          />
+        )}
       </div>
     </div>
   );
