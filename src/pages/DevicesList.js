553e246c1579e33b6c54c2860dddb801a8264106
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-09-03 11:13:33 +0530

    add popup handling

--- src/pages/DevicesList.js
+++ src/pages/DevicesList.js
@@ -1,15 +1,19 @@
 import { Helmet } from 'react-helmet-async';
 import { useEffect, useState } from 'react';
 
+// Components
+import SensorPopup from './SensorPop';
+
 export default function DevicesList() {
   const [devices, setDevices] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedDevice, setSelectedDevice] = useState(null);
 
   const fetchDevicesList = async () => {
     try {
       const response = await fetch("http://localhost:4001/api/get-devices", {
         method: "GET",
-        headers: {user_id: 5, device_id: 1}
+        headers: { user_id: 5, device_id: 1 }
       });
       const jsonData = await response.json();
       setDevices(jsonData);
@@ -24,29 +28,44 @@ export default function DevicesList() {
     fetchDevicesList();
   }, []);
 
+  const openSensorPopup = (deviceId) => {
+    setSelectedDevice(deviceId);
+  };
+
+  const closeSensorPopup = () => {
+    setSelectedDevice(null);
+  };
+
   const RenderDevicesList = () => {
     return (
       <>
         <Helmet>
           <title> Sensor values </title>
         </Helmet>
-        
+
         {loading ? (
-        <h1>Loading...</h1>
-      ) : devices.length > 0 ? (
-        <ul className="">
-          {devices.map((sv) => (
-            <button key={sv.device_id}>{sv.description}</button>
-          ))}
-        </ul>
-      ) : (
-        <h1>No devices</h1>
-      )}
+          <h1>Loading...</h1>
+        ) : devices.length > 0 ? (
+          <ul className="">
+            {devices.map((sv) => (
+              <button
+                key={sv.device_id}
+                onClick={() => openSensorPopup(sv.device_id)}
+              >{sv.description}</button>
+            ))}
+          </ul>
+        ) : (
+          <h1>No devices</h1>
+        )}
+
+        {selectedDevice && (
+          <SensorPopup deviceId={selectedDevice} onClose={closeSensorPopup} />
+        )}
       </>
     );
   };
 
-  
+
 
   return (
     <>
