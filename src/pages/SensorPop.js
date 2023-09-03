97acf2dc525a086832ae66772a5cc2060e7d2b56
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-09-03 19:45:52 +0530

    integrate sensorvalue

--- src/pages/SensorPop.js
+++ src/pages/SensorPop.js
@@ -1,30 +1,55 @@
 import React, { useEffect, useState } from 'react';
 
 
-
 const SensorPopup = ({ deviceId, onClose }) => {
     const [loading, setLoading] = useState(true)
     const [sensorParams, setSensorParams] = useState([])
+    const [sensorValues, setSensorValues] = useState([])
 
-    const fetchSensorValues = async () => {
+    const fetchSensorParams = async () => {
         try {
           const response = await fetch("http://localhost:4001/api/get-sensor-params", {
             method: "GET",
-            headers: { device_id:   deviceId }
+            headers: { device_id: deviceId }
           });
           const jsonData = await response.json();
           setSensorParams(jsonData);
-          console.log(jsonData);
+          // console.log(jsonData);
         } catch (err) {
           console.error(err.message);
         } finally {
           setLoading(false);
         }
       };
+
+      const fetchSensorValue = async(sensorId) => {
+        try {
+          const response = await fetch("http://localhost:4001/api/get-sensor-value", {
+            method: "GET",
+            headers: { sensor_id: sensorId }
+          });
+          const jsonData = await response.json();
+          setSensorValues(jsonData);
+          // console.log(jsonData[0].value);
+          console.log(sensorValues[0].value);
+          alert(sensorValues[0].value)
+        } catch (err) {
+          console.error(err.message);
+        } 
+      }
     
       useEffect(() => {
-        fetchSensorValues();
+        fetchSensorParams();
       }, []);
+
+      useEffect(() => {
+        // This effect will be triggered whenever sensorValues changes
+        if (sensorValues.length > 0) {
+          console.log(sensorValues[0].value);
+          alert(sensorValues[0].value);
+        }
+      }, [sensorValues]);
+    
   return (
     <div className="popup">
       <div className="popup-content">
@@ -34,9 +59,10 @@ const SensorPopup = ({ deviceId, onClose }) => {
         ) : sensorParams.length > 0 ? (
           <ul className="">
             {sensorParams.map((sv) => (
-              <li
+              <button
                 key={sv.sensor_id}
-              >{sv.key}</li>
+                onClick={() => fetchSensorValue(sv.sensor_id)}
+              >{sv.key}</button>
             ))}
           </ul>
         ) : (
