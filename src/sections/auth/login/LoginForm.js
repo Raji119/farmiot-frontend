ec1b97551caa6f6b60a8663ebfbe0697b20b53ab
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-09-06 19:42:14 +0530

    Add login functionality with cookies and jwt auth

--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 // @mui
 import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+
+import Cookies from 'js-cookie';
 // components
 import Iconify from '../../../components/iconify';
 
@@ -11,20 +13,73 @@ import Iconify from '../../../components/iconify';
 export default function LoginForm() {
   const navigate = useNavigate();
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleClick = () => {
-    navigate('/dashboard', { replace: true });
+  const handleClick = async() => {
+    try {
+      const response = await fetch('http://localhost:4001/api/login', {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ email, password })
+      })
+
+      if(response.status === 200){
+        const jsonData = await response.json();
+        if(jsonData.token){
+          Cookies.set('token', jsonData.token, {expires: 2})
+          // setCookie();
+          navigate('/dashboard', { replace: true });
+        } 
+      } else {
+        alert("Wrong email or password")
+      }
+      
+    } catch (err) {
+      console.log(err.message);
+    }
   };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const onWebLoad =async() => {
+    const token = Cookies.get('token');
+    console.log(token)
+    if (token) {
+      const response = await fetch('http://localhost:4001/api/authenticate', {
+        method: "GET",
+        headers: { "Content-Type": "application/json", "Authorization": token },
+      })
+
+      console.log(response);
+
+      if (response.status === 200) {
+        navigate('/dashboard/app', { replace: true });
+      }
+    }
+  } 
+
+  useEffect(()=>{
+    onWebLoad();
+  },[])
 
   return (
     <>
       <Stack spacing={3}>
-        <TextField name="email" label="Email address" />
+        <TextField name="email" label="Email address" value={email} onChange={handleEmailChange} />
 
         <TextField
           name="password"
           label="Password"
+          value={password}
+          onChange={handlePasswordChange}
           type={showPassword ? 'text' : 'password'}
           InputProps={{
             endAdornment: (
